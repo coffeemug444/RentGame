cd7d6c8a4aa70ae1e04add059f5c0cc5ab736204
Ben Slattery <ben@benslattery.com>
2023-05-21 01:19:51 +1200

    updated threading policy, again
    X11 requires that all calls to the window API be done from the main thread,
    unless you go through a little bit of extra effort with linking against X11
    and calling XInitThreads. I didn't want to do that so I did this instead,
    adding event queues for the UI.
    
    I don't plan on having event queues for the game logic, instead I'll have the
    ui directly call the game logic functions to update data. This way the UI
    won't have to wait until the next gametick to be in sync with what's in the
    game logic thread.

--- src/game.cpp
+++ src/game.cpp
@@ -12,47 +12,60 @@ Game::Game()
 :m_window(sf::VideoMode(400, 400), "Rent game")
 ,m_ui(*this, m_window.getSize())
 {
-}
-
-void Game::closeWindow()
-{
-   std::lock_guard lock(m_ui_mutex);
-   m_window.close();
+   m_cursor.loadFromSystem(sf::Cursor::Arrow);
+   m_window.setMouseCursor(m_cursor);
 }
 
 void Game::resizeWindow()
 {
-   std::lock_guard lock(m_ui_mutex);
+   if (not m_window_resized) return;
+   m_window_resized = false;
    float width = static_cast<float>(m_window.getSize().x);
    float height = static_cast<float>(m_window.getSize().y);
    m_window.setView(sf::View(sf::FloatRect{0.f,0.f,width, height}));
    m_ui.setScreenSize(m_window.getSize());
 }
 
-void Game::mouseMoved(sf::Vector2i mouse_pos)
+void Game::mouseMoved()
 {
-   if (mouse_pos.x < 0 || mouse_pos.y < 0) return;
-   
-   auto window_size = m_window.getSize();
-   if (mouse_pos.x > window_size.x) return;
-   if (mouse_pos.y > window_size.y) return;
+   while (m_ev_mouse_moved.size() > 0)
+   {
+      sf::Vector2i mouse_pos = m_ev_mouse_moved.front();
+      m_ev_mouse_moved.pop_front();
 
-   std::lock_guard lock(m_ui_mutex);
-   auto cursor_type = m_ui.mouseMoved(mouse_pos);
-   sf::Cursor cursor;
-   cursor.loadFromSystem(cursor_type);
-   m_window.setMouseCursor(cursor);
+      if (mouse_pos.x < 0 || mouse_pos.y < 0) return;
+      
+      auto window_size = m_window.getSize();
+      if (mouse_pos.x > window_size.x) return;
+      if (mouse_pos.y > window_size.y) return;
+
+      auto cursor_type = m_ui.mouseMoved(mouse_pos);
+      m_cursor.loadFromSystem(cursor_type);
+      m_window.setMouseCursor(m_cursor);
+   }
 }
 
-void Game::mouseDown(sf::Vector2i mouse_pos)
+void Game::mouseDown()
 {
-   m_ui.mouseDown(mouse_pos);
+   while (m_ev_mouse_down.size() > 0)
+   {
+      sf::Vector2i mouse_pos = m_ev_mouse_down.front();
+      m_ev_mouse_down.pop_front();
+
+      m_ui.mouseDown(mouse_pos);
+   }
 }
 
-void Game::mouseUp(sf::Vector2i mouse_pos)
+void Game::mouseUp()
 {
-   m_ui.mouseUp(mouse_pos);
-   mouseMoved(mouse_pos);
+   while (m_ev_mouse_up.size() > 0)
+   {
+      sf::Vector2i mouse_pos = m_ev_mouse_up.front();
+      m_ev_mouse_up.pop_front();
+      
+      m_ui.mouseUp(mouse_pos);
+      m_ev_mouse_moved.push_back(mouse_pos);
+   }
 }
 
 
@@ -64,21 +77,21 @@ void Game::pollEvents()
       switch (event.type)
       {
       case sf::Event::Closed:
-         closeWindow();
+         m_window.close();
          break;
       case sf::Event::Resized:
-         resizeWindow();
+         m_window_resized = true;
          break;
       case sf::Event::MouseMoved:
-         mouseMoved({event.mouseMove.x,event.mouseMove.y});
+         m_ev_mouse_moved.push_back({event.mouseMove.x,event.mouseMove.y});
          break;
       case sf::Event::MouseButtonPressed:
          if (event.mouseButton.button != sf::Mouse::Button::Left) break;
-         mouseDown({event.mouseButton.x,event.mouseButton.y});
+         m_ev_mouse_down.push_back({event.mouseButton.x,event.mouseButton.y});
          break;
       case sf::Event::MouseButtonReleased:
          if (event.mouseButton.button != sf::Mouse::Button::Left) break;
-         mouseUp({event.mouseButton.x,event.mouseButton.y});
+         m_ev_mouse_up.push_back({event.mouseButton.x,event.mouseButton.y});
          break;
       default:
          break;
@@ -86,40 +99,50 @@ void Game::pollEvents()
    }
 }
 
-void Game::displayLoop()
+void Game::uiLoop()
 {
    while (1)
    {
-      { std::lock_guard lock(m_ui_mutex);
-
-      if (not m_window.isOpen()) return;
-      
-      m_window.clear();
-
-      // draw scene
-      m_window.draw(m_ui);
-
-      m_window.display();
-
-      } // end mutex lock
-   
-      sleep_for(16ms);
+      {std::lock_guard lock(m_ui_mutex);
+      if (not m_window.isOpen()) break;
+      resizeWindow();
+      mouseMoved();
+      mouseDown();
+      mouseUp();
+      } // mutex lock
+      sleep_for(1ms);
    }
 }
 
 void Game::mainLoop()
 {
-   m_display_thread = std::async(&Game::displayLoop, this);
+   m_ui_thread = std::async(&Game::uiLoop, this);
+   sf::Clock timer;
+
+   // 1/60 seconds in microseconds, delay time for 60fps
+   sf::Int64 ds = 16667;
+
+   sf::Int64 last_time = timer.getElapsedTime().asMicroseconds() - ds - 1;
+
    while (1)
    {
       {std::lock_guard lock(m_ui_mutex);
-
       if (not m_window.isOpen()) break;
-      }
+
       pollEvents();
+      sf::Int64 current_time = timer.getElapsedTime().asMicroseconds();
+      if (current_time - last_time < ds) continue;
+      last_time = current_time;
+
+      m_window.clear();
+      // draw scene
+      m_window.draw(m_ui);
+      m_window.display();
+      } // mutex lock
+      sleep_for(1ms);
    }
 
-   m_display_thread.wait();
+   m_ui_thread.wait();
 }
 
 } // namespace Game
--- src/game.hpp
+++ src/game.hpp
@@ -2,15 +2,30 @@
 #include "SFML/Graphics.hpp"
 #include "display/ui.hpp"
 #include <future>
+#include <deque>
 
 /*
 
-Only set the ui mutex when making changes to the window object, and ensure
-it's released immediately after. Methods like `mouseUp` and `mouseDown` may
-eventually go to methods that change the window object, but the mutex gets
-grabbed there, otherwise the program will deadlock
+Main executing thread must be the one to poll the window. This includes events
+and draw calls. This diagram shows which loops access which mutexes. From what
+I understand you need a loop of mutex calls for deadlock to occur, and there 
+are no loops in this diagram so I think this is safe.
 
-*/
+               main loop
+                 +->-+
+                 |   |
+                 +---+ 
+game loop           \     ui loop
+  +->-+              \     +->-+  
+  |   |               ---->|   |
+  |   |<-------------------|   |
+  |   |                    |   |
+  +---+   +------------+   +---+  
+    \     | observable |     /      
+     ---->|   data     |<----
+          +------------+
+
+*/    
 
 
 namespace Game
@@ -22,14 +37,22 @@ public:
    Game();
    void mainLoop();
 private:
-   void displayLoop();
+   void uiLoop();
    void pollEvents();
    void closeWindow();
    void resizeWindow();
-   void mouseMoved(sf::Vector2i mouse_pos);
-   void mouseDown(sf::Vector2i mouse_pos);
-   void mouseUp(sf::Vector2i mouse_pos);
-   std::future<void> m_display_thread;
+   void mouseMoved();
+   void mouseDown();
+   void mouseUp();
+
+   bool m_window_resized;
+   std::deque<sf::Vector2i> m_ev_mouse_moved;
+   std::deque<sf::Vector2i> m_ev_mouse_down;
+   std::deque<sf::Vector2i> m_ev_mouse_up;
+
+   sf::Cursor m_cursor;
+
+   std::future<void> m_ui_thread;
    mutable std::mutex m_ui_mutex;
    sf::RenderWindow m_window;
    Ui m_ui;
