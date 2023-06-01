b201371df821c606db36fe341b6c35ab0142634c
Ben Slattery <ben.slattery@taitcommunications.com>
2023-06-02 11:30:27 +1200

    more additions to the input box

--- src/display/buttons/inputBoxNumberField.cpp
+++ src/display/buttons/inputBoxNumberField.cpp
@@ -5,16 +5,25 @@
 namespace Game
 {
 
-InputBoxNumberField::InputBoxNumberField()
+InputBoxNumberField::InputBoxNumberField(sf::Color background_colour, sf::Color text_colour, int font_size, int max_len)
 :m_number("0")
+,m_max_len(max_len)
 {
    m_number_display.setFont(OD::font);
    setDisplay();
+
+   m_background_box.setFillColor(background_colour);
+   m_number_display.setFillColor(text_colour); 
+
+   m_number_display.setCharacterSize(font_size);
+   m_background_box.setSize({font_size*max_len*0.7f, font_size*1.4f});
+
+   m_text_offset = sf::Vector2f{0.1f*font_size,0.1f*font_size};
 }
 
 void InputBoxNumberField::addDigit(char digit)
 {
-   if (m_number.size() == 10) return;
+   if (m_number.size() == m_max_len) return;
    if (m_number == "0") m_number = digit;
    else m_number.push_back(digit);
    setDisplay();
@@ -33,6 +42,12 @@ int InputBoxNumberField::getNumber() const
    return std::stoi(m_number);
 }
 
+void InputBoxNumberField::setPosition(const sf::Vector2f& pos) 
+{ 
+   m_background_box.setPosition(pos);
+   m_number_display.setPosition(pos+m_text_offset); 
+}
+
 void InputBoxNumberField::setDisplay()
 {
    m_number_display.setString(m_number);
@@ -40,6 +55,7 @@ void InputBoxNumberField::setDisplay()
 
 void InputBoxNumberField::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
+   target.draw(m_background_box);
    target.draw(m_number_display);
 }
    
--- src/display/buttons/inputBoxNumberField.hpp
+++ src/display/buttons/inputBoxNumberField.hpp
@@ -9,16 +9,19 @@ namespace Game
 class InputBoxNumberField : public sf::Drawable
 {
 public:
-   InputBoxNumberField();
+   InputBoxNumberField(sf::Color background_colour, sf::Color text_colour, int font_size, int max_len = 10);
    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
    void addDigit(char digit);
    void backSpace();
    int getNumber() const;
-   void setPosition(const sf::Vector2f& pos) { m_number_display.setPosition(pos); }
+   void setPosition(const sf::Vector2f& pos);
 private:
    void setDisplay();
    std::string m_number;
    sf::Text m_number_display;
+   sf::RectangleShape m_background_box;
+   int m_max_len;
+   sf::Vector2f m_text_offset;
 };
 
    
