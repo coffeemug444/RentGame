85692053e291f9df1dcde717573ed889c94c8881
Ben Slattery <ben.slattery@taitcommunications.com>
2023-07-03 17:01:48 +1200

    add some todo stuff for the market

--- src/logic/market.hpp
+++ src/logic/market.hpp
@@ -1,16 +1,28 @@
 #pragma once
-
+#include "property.hpp"
 
 namespace Game
 {
 
 
+// to be in charge of determining supply and demand of housing
+//
+// determines "going price" of houses, which is a basis for checking
+// if a house for sale will be sold, and is used for offering new houses
+// for sale
+//
+// determines rental demand and the current "going price" of rentals for
+// different house ages
 
 class Market
 {
 public:
 Market() {}
+int propertySalePrice(AgeClass age) const;
+int propertyRentalPrice(AgeClass age) const;
 
+private:
+// create new property
 };
 
 
