eeb003983e481ecd990011fbeefb7636b70631e6
Ben Slattery <ben.slattery@taitcommunications.com>
2023-07-04 17:08:52 +1200

    added TODO messages for marketListingWidget

--- src/display/widgets/marketListingWidget.cpp
+++ src/display/widgets/marketListingWidget.cpp
@@ -4,7 +4,47 @@ namespace Game
 {
 
 
+void MarketListingWidget::handleClick(int button_id) 
+{
+   switch (button_id)
+   {
+   case BUY_PROPERTY:
+      // TODO: send event to purchase the property with this ID
+      break;
+   default:
+      break;
+   }
+}
 
+// TODO: get the total size from top-left to bottom-right
+sf::Vector2f MarketListingWidget::getSize() const 
+{
+   return {0,0}; 
+}
+
+// TODO: get the position of the lop-left-most element
+sf::Vector2f MarketListingWidget::getPosition() const 
+{
+   return {0,0}; 
+}
+
+// TODO: move around the elements in this widget based on the screen size
+void MarketListingWidget::setScreenSize(const sf::Vector2u& size) 
+{
+
+}
+
+// TODO: retrieve data for listing with this ID and update the fields
+void MarketListingWidget::dataSync() 
+{
+
+}
+
+// TODO: move all elements in this widget by `pos`
+void MarketListingWidget::move(const sf::Vector2f& pos) 
+{
+
+}
 
 
 
--- src/display/widgets/marketListingWidget.hpp
+++ src/display/widgets/marketListingWidget.hpp
@@ -10,16 +10,19 @@ public:
    MarketListingWidget(unsigned id) : m_id(id) {}
    unsigned getId() const { return m_id; }
 
-   // TODO: fill these out
-   void handleClick(int button_id) override {}                  //  <--- THESE NEED TO BE DONE
-   sf::Vector2f getSize() const override { return {0,0}; }      //  <--- THESE NEED TO BE DONE
-   sf::Vector2f getPosition() const override { return {0,0}; }  //  <--- THESE NEED TO BE DONE
-   void setScreenSize(const sf::Vector2u& size) override {};    //  <--- THESE NEED TO BE DONE
-   void dataSync() override {}                                  //  <--- THESE NEED TO BE DONE
-   void move(const sf::Vector2f& pos) override {};              //  <--- THESE NEED TO BE DONE
+   void handleClick(int button_id) override;
+   sf::Vector2f getSize() const override;
+   sf::Vector2f getPosition() const override;
+   void setScreenSize(const sf::Vector2u& size) override;
+   void dataSync() override;
+   void move(const sf::Vector2f& pos) override;
 
 private:
    unsigned m_id;
+   enum ButtonId {
+      NONE = 0,
+      BUY_PROPERTY
+   };
 };
    
 } // namespace Game
