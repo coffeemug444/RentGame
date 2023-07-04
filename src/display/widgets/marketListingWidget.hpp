cc81ea1522565c72f91bd2735e69479d38efc4f9
Ben Slattery <ben.slattery@taitcommunications.com>
2023-07-04 17:02:29 +1200

    added some more TODOs to marketListingWidget

--- src/display/widgets/marketListingWidget.hpp
+++ src/display/widgets/marketListingWidget.hpp
@@ -14,6 +14,9 @@ public:
    void handleClick(int button_id) override {}                  //  <--- THESE NEED TO BE DONE
    sf::Vector2f getSize() const override { return {0,0}; }      //  <--- THESE NEED TO BE DONE
    sf::Vector2f getPosition() const override { return {0,0}; }  //  <--- THESE NEED TO BE DONE
+   void setScreenSize(const sf::Vector2u& size) override {};    //  <--- THESE NEED TO BE DONE
+   void dataSync() override {}                                  //  <--- THESE NEED TO BE DONE
+   void move(const sf::Vector2f& pos) override {};              //  <--- THESE NEED TO BE DONE
 
 private:
    unsigned m_id;
